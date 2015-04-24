930f0d5ddf0df4f43bc0cc5dddd5018d4654aed7
Faith Aidamenbor <faith.aidamenbor@rdcaviation.com>
2015-04-24 13:00:14 +0100

    Added fast sort algorithm

---
+++ FastSort.js
@@ -0,0 +1,28 @@
+		// temporary holder of position and sort-value
+		var map = arrayToSort.map(function (e, i) {
+			if (typeof e[propertyToSortWith] === 'string') {
+				return { index: i, value: e[propertyToSortWith].toLowerCase() };                    
+			}
+			else {
+				return { index: i, value: e[propertyToSortWith] };
+			}
+			
+		})
+
+		// sorting the map containing the reduced values
+		map.sort(function (a, b) {
+			if (sortDirection === "ascending") {
+				return +(a.value > b.value) || +(a.value === b.value) - 1;
+			}
+			else {
+				return +(a.value < b.value) || +(a.value === b.value) - 1;
+			}
+			
+		});
+
+		// container for the resulting order
+		var result = map.map(function (e) {
+			return arrayToSort[e.index];
+		});
+		return result;
+	};
